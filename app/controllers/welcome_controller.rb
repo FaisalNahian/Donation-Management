ff157e06b9c6ffa678e6227640447ab634bd3fc6
Brian McQuay <brian@onomojo.com>
2010-09-12 19:54:02 -0400

    first pass of slicing psds

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @user = User.new
   end
   
   def terms_of_service
--- config/routes.rb
+++ config/routes.rb
@@ -43,6 +43,11 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :password_resets
 
   map.terms_of_service "/terms-of-service", :controller => 'welcome', :action => 'terms_of_service'
+  map.privacy "/privacy", :controller => 'welcome', :action => 'privacy'
+  map.how "/how-it-works", :controller => 'welcome', :action => 'how'
+  map.about "/about", :controller => 'welcome', :action => 'about'
+  map.involved "/get-involved", :controller => 'welcome', :action => 'involved'
+  map.contact "/contact", :controller => 'welcome', :action => 'contact'
   
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "welcome"
